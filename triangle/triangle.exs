1b2b87eee97d604d4c83891e41f1f85240e42030
Dominic Wong <dominic.wong.617@gmail.com>
2015-11-03 09:47:10 +0800

    triangle

--- triangle/triangle.exs
+++ triangle/triangle.exs
@@ -6,6 +6,28 @@ defmodule Triangle do
   """
   @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
   def kind(a, b, c) do
+    cond do
+      has_non_positive_side?(a, b, c)        -> { :error, "all side lengths must be positive" }
+      violates_triangle_inequality?(a, b, c) -> { :error, "side lengths violate triangle inequality" }
+      equilateral?(a, b, c)                  -> { :ok, :equilateral }
+      isosceles?(a, b, c)                    -> { :ok, :isosceles }
+      true                                   -> { :ok, :scalene }
+    end
+  end
 
+  defp has_non_positive_side?(a, b, c) do
+    Enum.any?([a, b, c], fn(side) -> side <= 0 end)
+  end
+
+  defp violates_triangle_inequality?(a, b, c) do
+    c >= a + b || b >= a + c || a >= b + c
+  end
+
+  defp equilateral?(a, b, c) do
+    a == b && b == c
+  end
+
+  defp isosceles?(a, b, c) do
+    !equilateral?(a, b, c) && (a == b || b == c || a == c)
   end
 end
